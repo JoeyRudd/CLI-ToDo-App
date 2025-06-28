66ebed2558e22f92f0c59383ea9268b39519c727
Joey Rudd <emailjoeyrudd@gmail.com>
2025-06-28 15:12:33 -0400

    Begin switching from csv to SQlite

---
+++ internal/db.go
@@ -0,0 +1,30 @@
+package internal
+
+import (
+	"database/sql"
+	_ "github.com/mattn/go-sqlite3" // Import the SQLite driver
+)
+
+func InitDB(dbPath string) (*sql.DB, error) {
+	// Open the database connection
+	db, err := sql.Open("sqlite3", dbPath)
+	if err != nil {
+		return nil, err
+	}
+
+	sql := `
+	CREATE TABLE IF NOT EXISTS tasks (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    description TEXT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    completed BOOLEAN DEFAULT FALSE
+	);
+	`
+
+	_, err = db.Exec(sql)
+	if err != nil {
+		db.Close()
+		return nil, err
+	}
+	return db, nil
+}
